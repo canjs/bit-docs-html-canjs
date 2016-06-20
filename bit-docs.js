1be7da9f1a55339349aaafd0dca906e3830c8e57
Justin Meyer <justinbmeyer@gmail.com>
2016-06-20 15:58:56 -0500

    updating bit-docs

--- bit-docs.js
+++ bit-docs.js
@@ -4,6 +4,7 @@ module.exports = function(bitDocs){
     dependencies[pkg.name] = pkg.version;
 
     bitDocs.register("html", {
-        templates: path.join(__dirname, "templates")
+        templates: path.join(__dirname, "templates"),
+        dependencies: dependencies
     });
 }
