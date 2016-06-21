8ad850b2222dc045a4b00a7bafb8920ea630a55e
Justin Meyer <justinbmeyer@gmail.com>
2016-06-21 09:45:55 -0500

    sorts objects

--- templates/helpers.js
+++ templates/helpers.js
@@ -233,16 +233,70 @@ function getLevels(docObject, childrenMap, index) {
         return children.map(function(docObject){
             return {
                 title: getTitle(docObject),
-                items: childrenMap[docObject.name]
+                items: (childrenMap[docObject.name] || []).sort(compareDocObjects)
             }
         });
     } else {
         var children = childrenMap[docObject.name];
         if(children && children.length) {
             return [{
-                items: children
+                items: (children || []).sort(compareDocObjects)
             }]
         }
 
     }
 }
+var compareDocObjects = function(child1, child2){
+
+	// put groups at the end
+	if(/group|prototype|static/i.test(child1.type)){
+		if(!/group|prototype|static/i.test(child2.type)){
+			return 1;
+		} else {
+			if(child1.type === "prototype"){
+				return -1
+			}
+			if(child2.type === "prototype"){
+				return 1
+			}
+			if(child1.type === "static"){
+				return -1
+			}
+			if(child2.type === "static"){
+				return 1
+			}
+
+		}
+	}
+	if(/prototype|static/i.test(child2.type)){
+		return -1;
+	}
+
+	if(typeof child1.order == "number"){
+		if(typeof child2.order == "number"){
+			// same order given?
+			if(child1.order == child2.order){
+				// sort by name
+				if(child1.name < child2.name){
+					return -1
+				}
+				return 1;
+			} else {
+				return child1.order - child2.order;
+			}
+
+		} else {
+			return -1;
+		}
+	} else {
+		if(typeof child2.order == "number"){
+			return 1;
+		} else {
+			// alphabetical
+			if(child1.name < child2.name){
+				return -1
+			}
+			return 1;
+		}
+	}
+};
