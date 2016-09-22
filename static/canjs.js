4dcbb18be88e5ec4f6ff363656c87dbfda9d3e64
Garrett Reed <garrett@garrettreed.co>
2016-09-22 14:46:58 -0400

    Add popstate listener to support back/forward navigation

--- static/canjs.js
+++ static/canjs.js
@@ -1,39 +1,46 @@
 require("./canjs.less!");
 $ = require("jquery");
 
+window.addEventListener('popstate', function(e) {
+	var href = window.location.href;
+	navigate(href);
+});
+
 $(document.body).on("click","a",function(ev){
 	// make sure we're in the right spot
-	if(this.href === "javascript://") {
+	if (this.href === "javascript://") {
 		return;
 	}
 	// this might need to change if we add other things
 	if(this.hostname === window.location.hostname) {
-		var node = $(this);
 		var href = this.href;
 		ev.preventDefault();
-		window.history.pushState(null, null, this.href);
-		$.ajax(href,{dataType: "text"}).then(function(content){
-			$('#right .bottom-right').scrollTop(0);
-
-			var $content = $(content.match(/<body>(\n|.)+<\/body>/g)[0]);
-
-			var nav = $content.find(".bottom-left>ul");
-			var article = $content.find("article");
-			var breadcrumb = $content.find(".breadcrumb");
-
-			$(".bottom-left>ul").replaceWith(nav);
-			$("article").replaceWith(article);
-			$(".breadcrumb").replaceWith(breadcrumb);
-			// find what I clicked on in the current nav ... try positioning it in the same place.
-
-			// go through every package and re-init
-			for(var packageName in window.PACKAGES) {
-				if(typeof PACKAGES[packageName] === "function") {
-					PACKAGES[packageName]();
-				}
-			}
-		}, function(){
-			debugger;
-		});
+		window.history.pushState(null, null, href);
+		navigate(href);
 	}
 });
+
+function navigate(href) {
+	$.ajax(href,{dataType: "text"}).then(function(content){
+		$('#right .bottom-right').scrollTop(0);
+		var $content = $(content.match(/<body>(\n|.)+<\/body>/g)[0]);
+
+		var nav = $content.find(".bottom-left>ul");
+		var article = $content.find("article");
+		var breadcrumb = $content.find(".breadcrumb");
+
+		$(".bottom-left>ul").replaceWith(nav);
+		$("article").replaceWith(article);
+		$(".breadcrumb").replaceWith(breadcrumb);
+		// find what I clicked on in the current nav ... try positioning it in the same place.
+
+		// go through every package and re-init
+		for(var packageName in window.PACKAGES) {
+			if(typeof PACKAGES[packageName] === "function") {
+				PACKAGES[packageName]();
+			}
+		}
+	}, function(){
+		debugger;
+	});
+}
