dcb4bf5c390376b275cb5f229b06ffcf6263e6d5
Garrett Reed <garrett@garrettreed.co>
2016-09-29 15:10:41 -0400

    Add hash compatibility to article navigation. Partial refactor.

--- static/canjs.js
+++ static/canjs.js
@@ -1,30 +1,33 @@
 require("./canjs.less!");
 $ = require("jquery");
 
-var outline,
-	$articleContainer,
+var $articleContainer,
 	$onThisPage,
+	$tableOfContents,
 	$onThisPageTitle,
 	$headers;
 
 // Init
 function init() {
-	outline = window.docObject && parseInt(window.docObject.outline);
 	$articleContainer = $('#right .bottom-right');
 	$onThisPage = $('.on-this-page');
 	$onThisPageTitle = $('.breadcrumb-dropdown a');
+	$tableOfContents = $('on-this-page-table ol');
 	$headers = getHeaders();
-
-	$articleContainer.scrollTop(0); // ensure scroll-to-top
 
 	// remove anything in the "On This Page" list
 	$onThisPage.empty();
 	// add items to on-this-page dropdown
-	console.log('doing the headers thing');
-	console.log($headers);
 	$.each($headers, function(index, header) {
-		$onThisPage.append("<li>"+$(header).html()+"</li>");
+		if (!header.id) {
+			header.id = generateId(header);
+		}
+		$onThisPage.append("<a href=#"+header.id+"><li>"+$(header).html()+"</li></a>");
 	});
+
+	var $currentHeader = $(window.location.hash);
+	scrollToElement($currentHeader);
+	setOnThisPageTitle($currentHeader.html());
 }
 init();
 
@@ -33,54 +36,44 @@ window.addEventListener('popstate', function(ev) {
 	navigate(window.location.href);
 });
 
-// Override link behavior
-$(document.body).on("click", "a", function(ev) {
-	// make sure we're in the right spot
-	if (this.href === "javascript://") {
-		return;
-	}
-	// this might need to change if we add other things
-	if (this.hostname === window.location.hostname) {
-		var href = this.href;
-		ev.preventDefault();
-		window.history.pushState(null, null, href);
-		navigate(href);
-	}
-});
-
-// Scroll to matching header after clicking on this page item
-$(document.body).on("click", ".on-this-page li", function(ev) {
-	var targetContent = $(ev.currentTarget).html(),
-		targetScroll;
-
-	$.each($('h2'), function(index, el) {
-		el = $(el);
-		if (el.html() === targetContent) {
-			targetScroll = el;
-		}
-	});
-
-	var pos = targetScroll.offset().top - $articleContainer.offset().top + $articleContainer.scrollTop();
-	$articleContainer.scrollTop(pos);
-});
-
 // Update the "On This Page" placeholder with header text at scroll position
 $articleContainer.on("scroll", function(ev) {
 	if ($articleContainer[0].scrollHeight - $articleContainer.scrollTop() === $articleContainer.outerHeight()) {
 		// Show last header if at bottom of page
 		var $header = $($headers[$headers.length-1]);
-		$onThisPageTitle.html($header.html());
+		setOnThisPageTitle($header.html());
 	} else {
 		// Otherwise, try to set to last header value before scroll position
 		var contOffsetTop = $articleContainer.offset().top,
 			headerContent;
 		$.each($headers, function(index, header) {
 			var $header = $(header);
-			if ($header.offset().top - contOffsetTop <= 0) {
+			var marginTop = parseInt($header.css('margin-top')) || 20;
+			if ($header.offset().top - marginTop - contOffsetTop <= 0) {
 				headerContent = $header.html();
 			}
 		});
-		$onThisPageTitle.html(headerContent || 'On this page');
+		setOnThisPageTitle(headerContent);
+	}
+});
+
+// Override link behavior
+$(document.body).on("click", "a", function(ev) {
+	// make sure we're in the right spot
+	if (this.href === "javascript://") {
+		return;
+	}
+
+	var href = this.href;
+	ev.preventDefault();
+	window.history.pushState(null, null, href);
+
+	if (this.hostname === window.location.hostname) {
+		if (this.pathname === window.location.pathname && window.location.hash) {
+			scrollToElement($(window.location.hash));
+		} else {
+			navigate(href);
+		}
 	}
 });
 
@@ -118,10 +111,31 @@ function navigate(href) {
 }
 
 function getHeaders() {
-	var outlineLevel = !isNaN() ? outline : 1,
+	var	outline = window.docObject && parseInt(window.docObject.outline),
+		outlineLevel = !isNaN() ? outline : 1,
 		headerArr = [];
 	for (var i = 1; i <= outlineLevel; i++) {
 		headerArr.push('h' + (outlineLevel + 1));
 	}
 	return $(headerArr.join(', '));
+}
+
+function generateId(element) {
+	var txt = element.textContent;
+	return txt.replace(/\s/g,"").replace(/[^\w]/g,"_");
+}
+
+function scrollToElement($element) {
+	if ($element.length) {
+		var topMargin = parseInt($element.css('margin-top')) || 20;
+		var pos = $element.offset().top - topMargin - $articleContainer.offset().top + $articleContainer.scrollTop();
+		$articleContainer.scrollTop(pos);
+	} else {
+		$articleContainer.scrollTop(0);
+	}
+	setOnThisPageTitle($element.html());
+}
+
+function setOnThisPageTitle(title) {
+	$onThisPageTitle.html(title || 'On this page');
 }
