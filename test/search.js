27d5f98502e5e6b1631350545308134bfa597168
imaustink <austin@austinkurpuis.com>
2017-06-19 09:16:31 -0700

    Build fails without 1 tests

--- test/search.js
+++ test/search.js
@@ -1,37 +1,37 @@
-// var QUnit = require('steal-qunit');
-// var SearchControl = require('../static/search');
+var QUnit = require('steal-qunit');
+var SearchControl = require('../static/search');
 
-// /* Helper function for finding a specific result */
-// var indexOfPageInResults = function(pageName, results) {
-//   return results.findIndex(function(result) {
-//     return result.name === pageName;
-//   });
-// };
+/* Helper function for finding a specific result */
+var indexOfPageInResults = function(pageName, results) {
+  return results.findIndex(function(result) {
+    return result.name === pageName;
+  });
+};
 
-// /* Clear local storage */
-// window.localStorage.clear();
+/* Clear local storage */
+window.localStorage.clear();
 
-// /* Create the search bar element */
-// var searchBar = document.createElement('div');
-// searchBar.id = 'search-bar';
-// document.body.appendChild(searchBar);
+/* Create the search bar element */
+var searchBar = document.createElement('div');
+searchBar.id = 'search-bar';
+document.body.appendChild(searchBar);
 
-// /* Create a new instance of the search control */
-// var search = new SearchControl('#search-bar', {
-//   pathPrefix: '../temp'
-// });
+/* Create a new instance of the search control */
+var search = new SearchControl('#search-bar', {
+  pathPrefix: '../temp'
+});
 
-// /* Tests */
-// QUnit.module('search control');
+/* Tests */
+QUnit.module('search control');
 
-// QUnit.test('Search for “about”', function(assert) {
-//   var done = assert.async();
-//   search.searchEngineSearch('about').then(function(results) {
-//     assert.equal(results.length > 0, true, 'got results');
-//     assert.equal(indexOfPageInResults('about', results), 0, 'first result is the About page');
-//     done();
-//   });
-// });
+QUnit.test('Search for “about”', function(assert) {
+  var done = assert.async();
+  search.searchEngineSearch('about').then(function(results) {
+    assert.equal(results.length > 0, true, 'got results');
+    assert.equal(indexOfPageInResults('about', results), 0, 'first result is the About page');
+    done();
+  });
+});
 
 // QUnit.test('Search for “can-component”', function(assert) {
 //   var done = assert.async();
