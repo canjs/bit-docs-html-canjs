dea7e0861db259cb1beff70136e3841d8c6ac8f2
imaustink <austin@austinkurpuis.com>
2017-06-29 13:44:13 -0700

    Fix #358 Search results are broken in Safari

--- static/search.js
+++ static/search.js
@@ -377,7 +377,8 @@ var Search = Control.extend({
 				var split = searchTerm.split(lunr.tokenizer.separator);
 				split.forEach(function(term) {
 					q.term(term, { usePipeline: false, fields: q.allFields, boost: 10 });
-					q.term(term, { usePipeline: false, fields: q.allFields, wildcard: lunr.Query.wildcard.TRAILING });
+					// This line breaks Safari and seems to be useless
+					// q.term(term, { usePipeline: false, fields: q.allFields, wildcard: lunr.Query.wildcard.TRAILING });
 				});
 			});
 
