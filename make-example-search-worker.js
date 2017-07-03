6b780b10a01455a2c0fd2045f9603202c76a6680
imaustink <austin@austinkurpuis.com>
2017-07-03 12:38:44 -0700

    Fixed steal errors inside search-worker

--- make-example-search-worker.js
+++ make-example-search-worker.js
@@ -5,21 +5,40 @@ var stealTools = require("steal-tools");
 var forceBuild = process.argv.indexOf("-f") !== -1;
 
 var siteConfig = {
-  debug: true,
+  debug: false,
   dest: path.join(__dirname, 'doc', 'workers'),
-  minifyBuild: false
+  minifyBuild: true
 };
 
+var dest = siteConfig.dest + '/static/search-worker.js';
+
 fs.mkdir(siteConfig.dest, function() {
-  stealTools.build({
-    bundlesPath: siteConfig.dest,
-    config: __dirname + '/package.json!npm',
-    main: 'static/search-worker'
-  }, {
-    bundleAssets: true,
-    bundleSteal: true,
-    debug: siteConfig.debug ?  true : false,
-    minify: siteConfig.minifyBuild === false ? false : true,
-    quiet: siteConfig.debug ? false : true
+  stealTools.export({
+    system: {
+      config: __dirname + '/package.json!npm',
+      main: 'static/search-worker'
+    },
+    options: {
+      bundleAssets: true,
+      bundleSteal: true,
+      debug: siteConfig.debug ?  true : false,
+      minify: siteConfig.minifyBuild === false ? false : true,
+      quiet: siteConfig.debug ? false : true
+    },
+    outputs: {
+      "+standalone" : {
+        dest: dest
+      }
+    }
+  }).then(function(){
+    console.log('Replacing "window" with "self"');
+    fs.readFile(dest, 'utf8', function(err, file){
+      if(err) return console.error(err);
+      file = file.replace(/window/gmi, 'self');
+      fs.writeFile(dest, file, function(err){
+        if(err) return console.error(err);
+        console.log('Done');
+      });
+    });
   });
 });
