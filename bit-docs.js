b627bae8b7ef1a4e36c7a8d3575f47e9b348e4af
Austin Kurpuis <austin@austinkurpuis.com>
2017-07-11 16:40:12 -0700

    Fix ugly build setup (#376)
    * Update build process to use staticDist
    
    * Fixed referance to search-worker.js
    
    * Pre-release
    
    * Fixed build script
    
    * Update version number
    
    * Fix path to static dist
    
    * Update version number
    
    * Updated bit-docs-generate-html

--- bit-docs.js
+++ bit-docs.js
@@ -8,7 +8,8 @@ module.exports = function(bitDocs){
 
     bitDocs.register("html", {
         templates: path.join(__dirname, "templates"),
-        dependencies: dependencies
+        dependencies: dependencies,
+        staticDist: path.join(__dirname, "dist", "static")
     });
 
     bitDocs.register("tags", tags);
