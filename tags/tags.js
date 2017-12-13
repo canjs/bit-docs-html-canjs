857e5083b59dc42d1016d5b260b7747db38d0b5f
Justin Meyer <justinbmeyer@gmail.com>
2017-12-13 11:10:05 -0600

    Adds collection and outline to use default behavior

--- tags/tags.js
+++ tags/tags.js
@@ -1,3 +1,13 @@
 module.exports = {
-    subchildren: require('./subchildren')
+    subchildren: require('./subchildren'),
+    collection: {
+        add: function( line, curData, scope, objects, currentWrite, siteConfig ){
+            return siteConfig.tags._default.add.apply(this, arguments);
+        }
+    },
+    outline: {
+        add: function( line, curData, scope, objects, currentWrite, siteConfig ){
+            return siteConfig.tags._default.add.apply(this, arguments);
+        }
+    }
 };
