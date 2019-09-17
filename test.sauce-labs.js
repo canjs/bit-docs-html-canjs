fc1898d050dadc226eae693c3b69a5c2f71e2ea3
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-09-17 15:26:11 +0100

    Remove sauclabs timeout

--- test.sauce-labs.js
+++ test.sauce-labs.js
@@ -1,48 +1,29 @@
 const testSauceLabs = require('test-saucelabs');
 
-var maxDuration = 10800; // seconds, default 1800, max 10800
-var commandTimeout = 600; // seconds, default 300, max 600
-var idleTimeout = 1000; // seconds, default 90, max 1000
-
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
 
 testSauceLabs({
 	platforms: [{
 		browserName: 'MicrosoftEdge',
-		platform: 'Windows 10',
-		maxDuration: maxDuration,
-		commandTimeout: commandTimeout,
-		idleTimeout: idleTimeout
+		platform: 'Windows 10'
 	}, {
 		browserName: 'Safari',
 		'appium-version': '1.9.1',
 		platformName: 'iOS',
 		platformVersion: '11.0',
-		deviceName: 'iPhone 8 Simulator',
-		maxDuration: maxDuration,
-		commandTimeout: commandTimeout,
-		idleTimeout: idleTimeout
+		deviceName: 'iPhone 8 Simulator'
 	}, {
 		browserName: 'firefox',
 		platform: 'Windows 10',
-		version: 'latest',
-		maxDuration: maxDuration,
-		commandTimeout: commandTimeout,
-		idleTimeout: idleTimeout
+		version: 'latest'
 	}, {
 		browserName: 'safari',
 		platform: 'OS X 10.13',
 		version: '11',
-		maxDuration: maxDuration,
-		commandTimeout: commandTimeout,
-		idleTimeout: idleTimeout
 	}, {
 		browserName: 'googlechrome',
 		platform: 'OS X 10.12',
-		version: 'latest',
-		maxDuration: maxDuration,
-		commandTimeout: commandTimeout,
-		idleTimeout: idleTimeout
+		version: 'latest'
 	}],
 	urls: [{
 		name: 'bit-docs-html-canjs',
