0e82d69b79b74377c73d8c98db3c5a1554255f74
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-09-17 15:01:18 +0100

    Add saucelabs timeouts

--- test.sauce-labs.js
+++ test.sauce-labs.js
@@ -1,29 +1,48 @@
 const testSauceLabs = require('test-saucelabs');
 
+var maxDuration = 10800; // seconds, default 1800, max 10800
+var commandTimeout = 600; // seconds, default 300, max 600
+var idleTimeout = 1000; // seconds, default 90, max 1000
+
 // https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
 
 testSauceLabs({
 	platforms: [{
 		browserName: 'MicrosoftEdge',
-		platform: 'Windows 10'
+		platform: 'Windows 10',
+		maxDuration: maxDuration,
+		commandTimeout: commandTimeout,
+		idleTimeout: idleTimeout
 	}, {
 		browserName: 'Safari',
 		'appium-version': '1.9.1',
 		platformName: 'iOS',
 		platformVersion: '11.0',
-		deviceName: 'iPhone 8 Simulator'
+		deviceName: 'iPhone 8 Simulator',
+		maxDuration: maxDuration,
+		commandTimeout: commandTimeout,
+		idleTimeout: idleTimeout
 	}, {
 		browserName: 'firefox',
 		platform: 'Windows 10',
-		version: 'latest'
+		version: 'latest',
+		maxDuration: maxDuration,
+		commandTimeout: commandTimeout,
+		idleTimeout: idleTimeout
 	}, {
 		browserName: 'safari',
 		platform: 'OS X 10.13',
 		version: '11',
+		maxDuration: maxDuration,
+		commandTimeout: commandTimeout,
+		idleTimeout: idleTimeout
 	}, {
 		browserName: 'googlechrome',
 		platform: 'OS X 10.12',
-		version: 'latest'
+		version: 'latest',
+		maxDuration: maxDuration,
+		commandTimeout: commandTimeout,
+		idleTimeout: idleTimeout
 	}],
 	urls: [{
 		name: 'bit-docs-html-canjs',
