3e8177a5133e0634fa4a5d26f137ed74bae78a1d
Chasen Le Hara <git@chasenlehara.com>
2019-11-14 13:20:25 -0600

    Fix the first test

--- test/scroll.js
+++ test/scroll.js
@@ -5,30 +5,40 @@ var $ = require("jquery");
 
 F.attach(QUnit);
 
-QUnit.module('canjs-scroll');
+QUnit.module('canjs-scroll', {
+  beforeEach: function() {
+
+    // Clear local storage
+    window.localStorage.clear();
+
+  }
+});
 
 QUnit.test('Scroll down and refresh the page', function(assert) {
-	var done = assert.async(1);
+	var done = assert.async();
+
+	// Open a page
 	F.open('../doc/guides/html.html', function() {
+		var offset = 100;
+
+		// Set the size so the page takes up some space
 		F.frame.height = 400;
 		F.frame.width = "100%";
 
-		var section = F("#Overview")[0];
-		var style = window.getComputedStyle(section);
-		var topMargin = Math.max(parseInt(style.paddingTop), 60);
-		var pos = section.offsetTop - topMargin - document.querySelector("body").offsetTop;
-		F.win.scroll("top", pos);
+		// Scroll down 100px
+		F.win.scroll("top", offset);
 
-		F("#Overview").wait(function(){
-			var element = this[0];
-			if (!element) {
-				return false;
-			}
-			var rect = section.getBoundingClientRect();
-			F.win.location.reload();
-			return utils.rectIntersectsWithWindow(rect, F.win)
-		}, function(){
-			assert.ok(true);
+		// Reload the page
+		F.win.location.reload();
+
+		// Wait for the page to be loaded
+		F("body").wait(function(){
+			// Check whether the element exists
+			return this[0] ? true : false;
+		}, function() {
+			// Get the amount the body has been scrolled
+			var bodyTopOffset = F("body")[0].getBoundingClientRect().top;
+			assert.equal(bodyTopOffset, 0 - offset);
 			done();
 		});
 	});
@@ -36,7 +46,7 @@ QUnit.test('Scroll down and refresh the page', function(assert) {
 });
 
 QUnit.test('Refresh after going to a specific section', function(assert) {
-	var done = assert.async(1);
+	var done = assert.async();
 	F.open('../doc/guides/html.html', function() {
 		F.frame.height = 400;
 		F.frame.width = "100%";
@@ -61,7 +71,7 @@ QUnit.test('Refresh after going to a specific section', function(assert) {
 });
 
 QUnit.test("Refresh after going to a specific section and scrolling", function(assert) {
-	var done = assert.async(1);
+	var done = assert.async();
 
 	F.open('../doc/guides/html.html', function() {
 		F.frame.height = 400;
